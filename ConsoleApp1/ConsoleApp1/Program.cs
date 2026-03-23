63bfe898d76d86e2cb7e748e6781c2e65cf2a496
PaineiD <a.vereshak@inbox.ru>
2026-03-23 20:37:56 +0300

    First commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello");
+    }
+}
+
+public static class Solution
+{
+    public static int NumberOfSteps(int num)
+    {
+        return 0;
+    }
+}
